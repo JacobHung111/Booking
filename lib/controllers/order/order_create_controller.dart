ca07f71c0cf92b32057b85c90eac1c4085eeaa42
Jacob Hung <jacobhys1998@gmail.com>
2023-09-18 19:39:51 -0400

    update DateTime Picker

--- lib/controllers/order/order_create_controller.dart
+++ lib/controllers/order/order_create_controller.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+
 import '../../models/order_create_model.dart';
 import '../common/loading_controller.dart';
 
 class OrderCreateController extends GetxController {
   var orderDetail = OrderCreateListModel().obs;
+
+  var dateTimeController = TextEditingController();
+
+  final DateFormat formatter = DateFormat('yyyy-MM-dd hh:mm');
 
   createOnClicked() {
     FocusManager.instance.primaryFocus?.unfocus();
--- lib/views/pages/order/order_create_page.dart
+++ lib/views/pages/order/order_create_page.dart
@@ -1,5 +1,8 @@
+import 'package:booking/commons/widgets/textbox.dart';
 import 'package:booking/controllers/order/order_create_controller.dart';
+import 'package:booking/views/dialogs/datetime_dialogs.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 
 class OrderCreatePage extends GetView<OrderCreateController> {
@@ -8,11 +11,47 @@ class OrderCreatePage extends GetView<OrderCreateController> {
 
   @override
   Widget build(BuildContext context) {
+    //Date Time Picker
+    List<Widget> dateTimePicker = [
+      IconButton(
+          onPressed: () => DateTimeDialogs.showDialogPicker(
+                  context, "Order Date", 30, controller.orderDetail.value.date,
+                  (value) {
+                controller.dateTimeController.text =
+                    controller.formatter.format(value);
+                controller.orderDetail.update((val) {
+                  val?.date = value;
+                });
+              }),
+          icon: const FaIcon(FontAwesomeIcons.calculator)),
+      IconButton(
+          onPressed: () => DateTimeDialogs.showDialogTimePicker(
+                  context, "Order Date", controller.orderDetail.value.date,
+                  (value) {
+                controller.dateTimeController.text =
+                    controller.formatter.format(value);
+                controller.orderDetail.update((val) {
+                  val?.date = value;
+                });
+              }),
+          icon: const FaIcon(FontAwesomeIcons.clock))
+    ];
+
     return Scaffold(
         appBar: AppBar(),
         body: ListView(
           padding: const EdgeInsets.all(10),
-          children: [],
+          children: [
+            TextField(
+                controller: controller.dateTimeController,
+                readOnly: true,
+                decoration: InputDecoration(
+                    counterText: '',
+                    hintText: "Order Date & Time",
+                    focusedBorder: const UnderlineInputBorder(),
+                    enabledBorder: const UnderlineInputBorder(),
+                    suffixIcon: Column(children: dateTimePicker)))
+          ],
         ));
   }
 }
